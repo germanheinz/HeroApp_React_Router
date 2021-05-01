e3a8bd7b0d7ed52e98bf1f631643493af6af788c
German Heinz <gerheinz@everis.com>
2021-05-01 12:51:38 +0200

    Adding Reducer, Context - Login

--- src/HeroeApp.js
+++ src/HeroeApp.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+import { AuthContext } from './components/auth/AuthContext'
+import { authReducer } from './components/auth/authReducer'
 import { AppRouter } from './components/routers/AppRouter'
 
+
+const init = () => {
+    return JSON.parse(localStorage.getItem('user')) || { logged: false }
+}
+
 export const HeroeApp = () => {
+
+    const [user, dispatch] = useReducer(authReducer, {}, init)
+
     return (
-       <AppRouter />
+    <AuthContext.Provider value={{ user, dispatch}}>
+        <AppRouter />
+    </AuthContext.Provider>
     )
 }
