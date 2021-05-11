b9464747c1f3787abeebfc04959273129338403a
German Heinz <gerheinz@everis.com>
2021-05-11 17:14:52 +0200

    testing router

---
+++ src/tests/routers/PrivateRoute.test.js
@@ -0,0 +1,38 @@
+import { React } from "react";
+import { MemoryRouter } from "react-router-dom";
+import { PrivateRoute } from '../../components/routers/PrivateRoute';
+import { mount } from 'enzyme';
+import Enzyme from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+ 
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('Pruebas en <PrivateRoute />', () => {
+
+    const props = {
+        location: {
+            pathname: '/marvel'
+        }
+    }
+    
+    test('Debe mostrar el componente si está auntenticado y guardar localStorage', () => {
+
+        const wrapper = mount(
+            <MemoryRouter>
+                <PrivateRoute
+                    isAuthenticated={true}
+                    component={ () => <span>hola</span>}
+                    {...props }
+                />
+            </MemoryRouter>
+
+        );
+        console.log(wrapper.html());
+
+        expect( wrapper.find('span').exists() ).toBe(true);
+
+        
+        
+    })
+    
+})
