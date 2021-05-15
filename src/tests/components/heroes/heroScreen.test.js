67b882c058873df3a98893d3d1c97a70ff841e50
German Heinz <gerheinz@everis.com>
2021-05-15 23:23:31 +0200

    Test - HeroScreen #2

--- src/tests/components/heroes/heroScreen.test.js
+++ src/tests/components/heroes/heroScreen.test.js
@@ -5,7 +5,7 @@ import { HeroScreen } from '../../../components/heroes/HeroScreen';
 import Enzyme from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import '@testing-library/jest-dom';
-import { MemoryRouter } from 'react-router';
+import { MemoryRouter, Route } from 'react-router';
 
  
 Enzyme.configure({ adapter: new Adapter() });
@@ -19,15 +19,51 @@ describe('Testing en <HeroScreen />', () => {
         goBack: jest.fn()
     }
 
-    const wrapper = mount(
-        <MemoryRouter>
-            <HeroScreen history={ history }/>
-        </MemoryRouter>
-    );
-
+    
     test('Debe de mostrar el componente redirect si no hay argumentos en el URL ', () => {
+        const wrapper = mount(
+            <MemoryRouter>
+                <HeroScreen history={ history }/>
+            </MemoryRouter>
+        );
         expect(wrapper.find('Redirect').exists()).toBe(true);
     })
+    
+    test('Debe mostrar un heroe si el parametro existe y se encuentra', () => {
+        const wrapper = mount(
+            <MemoryRouter initialEntries={['/hero/marvel-spider']}>
+               <Route path="/hero/:heroId" component={ HeroScreen } />
+            </MemoryRouter>
+        );
+
+        expect( wrapper.find('.card').exists()).toBe(true);
+
+    });
+
+    test('debe regresar a la pantalla anterior con PUSH', () => {
+
+        const history = {
+            length: 1,
+            push: jest.fn(),
+            goBack: jest.fn()
+        }
+
+        const wrapper = mount(
+            <MemoryRouter initialEntries={['/hero/marvel-spider']}>
+               <Route
+                    path="/hero/:heroId"
+                    component={ () => <HeroScreen history={ history } /> } 
+                />
+            </MemoryRouter>
+        );
+
+        wrapper.find('button').prop('onClick')();
+
+        expect( history.push ).toHaveBeenCalledWith('/');
+        expect( history.goBack ).not.toHaveBeenCalled();
+
+    })
+    
     
 
 })
